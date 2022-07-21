9ba6d7322879392c5c01a8e6474d1a7b9a95948f
Dev Macbook Pro 1 <devmacbookpro1@Devs-MacBook-Pro.local>
2022-07-21 08:41:24 +0100

    Debug Changes

--- debug.js
+++ debug.js
@@ -1,21 +1,17 @@
 const debug = {
   global_scope: () => {
-    // Grab browser's default global variables.
     const iframe = window.document.createElement("iframe");
     iframe.src = "about:blank";
     window.document.body.appendChild(iframe);
     const browserGlobals = Object.keys(iframe.contentWindow);
     window.document.body.removeChild(iframe);
 
-    // Get the global variables added at runtime by filtering out the browser's
-    // default global variables from the current window object.
     const runtimeGlobals = Object.keys(window).filter((key) => {
       const isFromBrowser = browserGlobals.includes(key);
       return !isFromBrowser;
     });
 
     console.log("Runtime globals", runtimeGlobals);
-
     return runtimeGlobals;
   },
 };
